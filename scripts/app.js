31669b35bc7cbad6e0761239e8d2a3d7b435ae4f
MalgorzataBorowy <61415965+MalgorzataBorowy@users.noreply.github.com>
2020-02-27 22:24:22 +0100

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -35,6 +35,36 @@ window.addEventListener('keypress',(ev) => {
     render()
 })
 
+// Klawiatura
+const keyLayout = [
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+    "a", "s", "d", "f", "g", "h", "j", "k", "l",
+    "z", "x", "c", "v", "b", "n", "m"
+]
+
+const keyboardKeys = document.createElement('div')
+
+keyLayout.forEach((key => {
+    const keyElement = document.createElement("button");
+    const insertLineBreak = ["p", "l"].indexOf(key) !== -1;
+    keyElement.setAttribute("type", "button");
+    keyElement.classList.add("keyboard__key");
+    keyElement.textContent = key
+    keyboardKeys.appendChild(keyElement)
+    if (insertLineBreak) {
+        keyboardKeys.appendChild(document.createElement("br"));
+    }
+    keyElement.addEventListener("click", (ev) => {
+        firstGame.makeGuess(ev.target.textContent)
+        render()
+        console.log(ev.target.textContent)
+    })
+}))
+
+document.querySelector('#keyboard-keys').appendChild(keyboardKeys)
+
+// Funkcja do renderowania hasła
+
 const render = () => {
     puzzleEl.innerHTML = ''
     guessesEl.textContent = firstGame.statusMessage
